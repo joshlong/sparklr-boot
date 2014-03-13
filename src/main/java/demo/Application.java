170405ac610d29b2f792c59091538e4790c624f2
Josh Long <josh@joshlong.com>
2014-03-13 07:09:02 -0700

    Adding a Spring Data REST-powered repository.
    This code shows a Spring Security OAuth-secured Spring Data REST API babysitting a DB. *That* was easy!

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -5,6 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -14,84 +18,109 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.OAuth2ResourceServerConfigurer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+/***
+ * this whole example was lifted wholesale from Dr. Dave Syer's epic
+ * @author Dave Syer
+ */
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-@RestController
+@Import(RepositoryRestMvcConfiguration.class)
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	@RequestMapping("/")
-	public String home() {
-		return "Hello World";
-	}
+    public static final String CRM_RESOURCE_ID = "crm";
 
-	@Configuration
-	@EnableResourceServer
-	protected static class ResourceServer extends ResourceServerConfigurerAdapter {
+    @Configuration
+    @EnableResourceServer
+    protected static class ResourceServer extends ResourceServerConfigurerAdapter {
 
-		@Override
-		public void configure(HttpSecurity http) throws Exception {
-			// @formatter:off
-			http
-				.requestMatchers().antMatchers("/","/admin/beans").and()
-				.authorizeRequests()
-				.anyRequest().access("#oauth2.hasScope('read')");
-			// @formatter:on
-		}
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            http
+                    .requestMatchers().antMatchers("/*", "/admin/beans").and()
+                    .authorizeRequests()
+                    .anyRequest().access("#oauth2.hasScope('read')");
+            // @formatter:on
+        }
 
-		@Override
-		public void configure(OAuth2ResourceServerConfigurer resources) throws Exception {
-			resources.resourceId("sparklr");
-		}
+        @Override
+        public void configure(OAuth2ResourceServerConfigurer resources) throws Exception {
+            resources.resourceId(CRM_RESOURCE_ID);
+        }
 
-	}
+    }
 
-	@Configuration
-	@EnableAuthorizationServer
-	protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 
-		@Autowired
-		private AuthenticationManager authenticationManager;
+        @Autowired
+        private AuthenticationManager authenticationManager;
 
-		@Override
-		public void configure(OAuth2AuthorizationServerConfigurer oauthServer) throws Exception {
-			oauthServer.authenticationManager(authenticationManager);
-		}
+        @Override
+        public void configure(OAuth2AuthorizationServerConfigurer oauthServer) throws Exception {
+            oauthServer.authenticationManager(authenticationManager);
+        }
 
-		@Override
-		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-			// @formatter:off
-		 	clients.inMemory()
-		        .withClient("my-trusted-client")
-		            .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
-		            .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
-		            .scopes("read", "write", "trust")
-		            .resourceIds("sparklr")
-		            .accessTokenValiditySeconds(60)
- 		    .and()
-		        .withClient("my-client-with-registered-redirect")
-		            .authorizedGrantTypes("authorization_code")
-		            .authorities("ROLE_CLIENT")
-		            .scopes("read", "trust")
-		            .resourceIds("sparklr")
-		            .redirectUris("http://anywhere?key=value")
- 		    .and()
-		        .withClient("my-client-with-secret")
-		            .authorizedGrantTypes("client_credentials", "password")
-		            .authorities("ROLE_CLIENT")
-		            .scopes("read")
-		            .resourceIds("sparklr")
-		            .secret("secret");
-		// @formatter:on
-		}
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            // @formatter:off
+            clients.inMemory()
+                    .withClient("ios-crm")
+                    .authorizedGrantTypes("client_credentials", "password")
+                    .authorities("ROLE_CLIENT")
+                    .scopes("read")
+                    .resourceIds(CRM_RESOURCE_ID)
+                    .secret("secret");
+            // @formatter:on
+        }
 
-	}
+    }
 
+}
+
+
+@RepositoryRestResource
+interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
+
+@Entity
+class Customer {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName, lastName;
+
+
+    Customer() {
+    } // for JPA
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
 }
