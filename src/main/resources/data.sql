8ae51bd2393be95196bef557cfe9c6c4bb1cf9a6
Josh Long <josh@joshlong.com>
2014-03-13 07:28:29 -0700

    adding data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into customer(first_name, last_name ) values ('Dave', 'Syer');
+insert into customer(first_name, last_name ) values ('Josh', 'Long');
+insert into customer(first_name, last_name ) values ('Rob', 'Winch');
+insert into customer(first_name, last_name ) values ('Juergen', 'Hoeller');
+insert into customer(first_name, last_name ) values ('Phillip', 'Webb');
+insert into customer(first_name, last_name ) values ('Roy', 'Clarkson');
+insert into customer(first_name, last_name ) values ('Andy', 'Cement');
